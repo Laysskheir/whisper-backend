19a0ca84feb8f3ca0007426c00731042ad68de5d
Layss kheir <laysskheir@gmail.com>
2023-12-20 15:06:56 +0200

    fix aws commit

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework_simplejwt",
     "drf_yasg",
-    "storages",
 ]
 
 MIDDLEWARE = [
@@ -181,7 +180,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 # AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
 
 
